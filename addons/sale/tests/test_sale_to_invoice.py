b188dc3b16a139a51a53ca428762e9ac40846a4a
Hiral Bhavsar <hbh@odoo.com>
2018-02-05 19:08:40 +0530

    [REF] sale: Removed existing testcase from test_sale_to_invoice python file and added three new testcases related to process of sale to invoice as listed below, 	- Invoice with downpayment. 	- Invoice with discount. 	- Invoice with refund.

--- addons/sale/tests/test_sale_to_invoice.py
+++ addons/sale/tests/test_sale_to_invoice.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from datetime import datetime
-
-from odoo.tools import mute_logger
+from odoo.tests import Form
+from odoo.tools import pycompat
 from .test_sale_common import TestCommonSaleNoChart
 
 
@@ -12,63 +11,129 @@ class TestSaleToInvoice(TestCommonSaleNoChart):
     @classmethod
     def setUpClass(cls):
         super(TestSaleToInvoice, cls).setUpClass()
+        cls.setUpClassicProducts()
         cls.setUpAdditionalAccounts()
         cls.setUpAccountJournal()
 
-    @mute_logger('odoo.addons.base.models.ir_model', 'odoo.osv.orm')
-    def setUp(self):
-        super(TestSaleToInvoice, self).setUp()
-
-    def test_sale_to_invoice(self):
-        """ Testing for invoice create,validate and pay with invoicing and payment user."""
-        # Usefull models
-        IrModelData = self.env['ir.model.data']
-        partner_obj = self.env['res.partner']
-        journal_obj = self.env['account.journal']
-        # Usefull record id
-        group_id = IrModelData.xmlid_to_res_id('account.group_account_invoice') or False
-        company_id = IrModelData.xmlid_to_res_id('base.main_company') or False
-
-        # Usefull accounts
-        account_recv_id = self.account_receivable
-        account_rev_id = self.account_revenue
-
-        # Add account to product
-        product_template_id = self.env.ref('sale.advance_product_0').product_tmpl_id
-        product_template_id.write({'property_account_income_id': account_rev_id})
-
-        # In order to test, I create new user and applied Invoicing & Payments group.
-        user = self.env['res.users'].create({
-            'name': 'Test User',
-            'login': 'test@test.com',
-            'company_id': 1,
-            'groups_id': [(6, 0, [group_id])]})
-        assert user, "User will not created."
-        # I create partner for sales order.
-        partner = partner_obj.create({
-            'name': 'Test Customer',
-            'email': 'testcustomer@test.com',
-            'property_account_receivable_id': account_recv_id,
+        # Create the sale order with four order lines
+        cls.sale_order = cls.env['sale.order'].create({
+            'partner_id': cls.partner_customer_usd.id,
+            'order_line': [
+                (0, 0, {'product_id': cls.product_order.id, 'product_uom_qty': 5.0}),
+                (0, 0, {'product_id': cls.service_deliver.id, 'product_uom_qty': 4.0}),
+                (0, 0, {'product_id': cls.service_order.id, 'product_uom_qty': 3.0}),
+                (0, 0, {'product_id': cls.product_deliver.id, 'product_uom_qty': 2.0}),
+            ]
         })
 
-        # In order to test I create sales order and confirmed it.
-        order = self.env['sale.order'].create({
-            'partner_id': partner.id,
-            'partner_invoice_id': partner.id,
-            'partner_shipping_id': partner.id,
-            'date_order': datetime.today(),
-            'pricelist_id': self.env.ref('product.list0').id})
-        assert order, "Sales order will not created."
-        context = {"active_model": 'sale.order', "active_ids": [order.id], "active_id": order.id}
-        order.with_context(context).action_confirm()
-        # Now I create invoice.
-        payment = self.env['sale.advance.payment.inv'].create({
-            'advance_payment_method': 'fixed',
-            'amount': 5,
-            'product_id': self.env.ref('sale.advance_product_0').id,
-        })
-        invoice = payment.with_context(context).create_invoices()
-        assert order.invoice_ids, "No any invoice is created for this sales order"
-        # Now I validate pay invoice wihth Test User(invoicing and payment).
-        for invoice in order.invoice_ids:
-            invoice.with_context(context).invoice_validate()
+        # Context
+        cls.context = {
+            'active_model': 'sale.order',
+            'active_ids': [cls.sale_order.id],
+            'active_id': cls.sale_order.id,
+            'default_journal_id': cls.journal_sale
+        }
+
+    def test_downpayment(self):
+        """ Test invoice with downpayment """
+        # Confirm the SO
+        self.sale_order.action_confirm()
+        # Let's do an invoice for a deposit of 100
+        payment_form = Form(self.env['sale.advance.payment.inv'].with_context(self.context))
+        payment_form.advance_payment_method = 'fixed'
+        payment_form.amount = 100
+        payment_form.deposit_account_id = self.account_income
+        payment = payment_form.save()
+        payment.create_invoices()
+
+        # Check that an invoice created or not
+        assert self.sale_order.invoice_ids, "An invoice should be created for this sales order"
+        downpayment_line = self.sale_order.order_line.filtered(lambda l: l.is_downpayment)
+        self.assertEquals(len(downpayment_line), 1, 'SO line downpayment should be created on SO')
+
+        with Form(self.sale_order) as order:
+            with order.order_line.edit(1) as line:
+                line.qty_delivered = 4
+            with order.order_line.edit(3) as line:
+                line.qty_delivered = 2
+
+        # Let's do an invoice with refunds
+        payment_form = Form(self.env['sale.advance.payment.inv'].with_context(self.context))
+        payment_form.advance_payment_method = 'all'
+        payment_form.deposit_account_id = self.account_income
+        payment = payment_form.save()
+        payment.create_invoices()
+
+        assert self.sale_order.invoice_ids, "An invoice should be created for this sales order"
+
+        invoice = self.sale_order.invoice_ids[0]
+        self.assertEquals(len(invoice.invoice_line_ids), len(self.sale_order.order_line), 'All lines should be invoiced')
+        self.assertEquals(invoice.amount_total, self.sale_order.amount_total - downpayment_line.price_unit, 'Downpayment should be applied')
+
+    def test_invoice_with_discount(self):
+        """ Test invoice with discount """
+        with Form(self.sale_order) as order:
+            with order.order_line.edit(0) as line:
+                line.discount = 20
+            with order.order_line.edit(1) as line:
+                line.discount = 20
+                line.qty_delivered = 4
+            with order.order_line.edit(2) as line:
+                line.discount = -10
+            with order.order_line.edit(3) as line:
+                line.qty_delivered = 2
+
+        for line in self.sale_order.order_line.filtered(lambda l: l.discount):
+            product_price = line.price_unit * line.product_uom_qty
+            self.assertEquals(line.discount, (product_price - line.price_subtotal)/product_price*100, 'Discount should be applied on order line')
+
+        self.sale_order.action_confirm()
+        # Let's do an invoice with invoiceable lines
+        payment_form = Form(self.env['sale.advance.payment.inv'].with_context(self.context))
+        payment_form.advance_payment_method = 'delivered'
+        payment = payment_form.save()
+        payment.create_invoices()
+
+        invoice = self.sale_order.invoice_ids[0]
+        invoice.action_invoice_open()
+
+        for line, inv_line in pycompat.izip(self.sale_order.order_line, invoice.invoice_line_ids):
+            self.assertEquals(line.discount, inv_line.discount, 'Discount on lines of order and invoice should be same')
+
+    def test_invoice_refund(self):
+        """ Test invoice with refund """
+        self.sale_order.action_confirm()
+        # Take only invoicable line
+        order_line = self.sale_order.order_line.filtered(lambda l: l.product_id.invoice_policy == 'order')
+
+        for line in order_line:
+            self.assertEquals(line.qty_to_invoice, line.product_uom_qty, 'Quantity to invoice should be same as ordered quantity')
+            self.assertEquals(line.qty_invoiced, 0.0, 'Invoiced quantity should be zero as no any invoice created for SO')
+
+        # Let's do an invoice with invoiceable lines
+        payment_form = Form(self.env['sale.advance.payment.inv'].with_context(self.context))
+        payment_form.advance_payment_method = 'delivered'
+        payment = payment_form.save()
+        payment.create_invoices()
+
+        invoice = self.sale_order.invoice_ids[0]
+
+        with Form(invoice) as inv:
+            with inv.invoice_line_ids.edit(0) as line:
+                line.quantity = 3
+            with inv.invoice_line_ids.edit(1) as line:
+                line.quantity = 2
+
+        invoice.action_invoice_open()
+        for line in order_line:
+            self.assertEquals(line.qty_to_invoice, line.product_uom_qty - line.qty_invoiced, 'Quantity to invoice should be a difference between ordered quantity and invoiced quantity')
+
+        # Make a credit note
+        credit_note_form = Form(self.env['account.invoice.refund'].with_context({'active_ids': [invoice.id], 'active_id': invoice.id}))
+        credit_note_form.filter_refund = 'refund'
+        credit_note_form.description = 'test'
+        credit_note = credit_note_form.save()
+        credit_note.invoice_refund()
+
+        invoice_1 = self.sale_order.invoice_ids[0]
+        invoice_1.action_invoice_open()
