ccf508b068bd0d27147df771bcea71e319580197
jem-odoo <jem@openerp.com>
2018-02-12 14:09:39 +0100

    movecode: data/demo

--- addons/purchase_stock/__manifest__.py
+++ addons/purchase_stock/__manifest__.py
@@ -11,8 +11,12 @@
     'website': 'https://www.odoo.com/page/purchase',
     'depends': ['stock_account', 'purchase'],
     'data': [
+        'data/purchase_stock_data.xml',
         'views/stock_views.xml',
         'report/purchase_report_views.xml',
+    ],
+    'demo': [
+        'data/purchase_stock_demo.xml',
     ],
     'installable': True,
     'auto_install': True,
