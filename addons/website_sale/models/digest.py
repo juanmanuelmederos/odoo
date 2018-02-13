9173dc93ef58991da0cd0fff05cb4c86f9cc4f8b
Siddarth Gajjar <sga@odoo.com>
2018-02-13 16:22:50 +0530

    [IMP] website_sale: implement ecommerce kpi for ecommerce digest

---
+++ addons/website_sale/models/digest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models
+
+
+class Digest(models.Model):
+    _inherit = 'digest.digest'
+
+    kpi_website_sale_total = fields.Boolean(string='eCommerce Sales')
+    kpi_website_sale_total_value = fields.Monetary(compute='_compute_kpi_website_sale_total_value')
+
+    @api.depends('start_date', 'end_date')
+    def _compute_kpi_website_sale_total_value(self):
+        context = self._context
+        for record in self:
+            date_domain = [("date_order", ">=", record.start_date), ("date_order", "<=", record.end_date)]
+            if context.get('timeframe') == 'yesterday':
+                date_domain = [("date_order", ">=", record.start_date), ("date_order", "<", record.end_date)]
+            date_domain += [('state', 'not in', ['draft', 'cancel', 'sent']), ('team_id.team_type', '=', 'website'), ('company_id', '=', context.get('company_id').id)]
+            confirmed_website_sales = self.env['sale.order'].search(date_domain)
+            record.kpi_website_sale_total_value = sum(confirmed_website_sales.mapped('amount_total'))
