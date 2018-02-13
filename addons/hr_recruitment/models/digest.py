76822a35d82b9b5385997fae96cd7cb1a50e58f7
Siddarth Gajjar <sga@odoo.com>
2018-02-13 16:21:32 +0530

    [IMP] hr_recruitment: implement recruitment kpi for recruitment digest

---
+++ addons/hr_recruitment/models/digest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models
+
+
+class Digest(models.Model):
+    _inherit = 'digest.digest'
+
+    kpi_hr_recruitment_new_colleagues = fields.Boolean(string='New Employees')
+    kpi_hr_recruitment_new_colleagues_value = fields.Integer(compute='_compute_kpi_hr_recruitment_new_colleagues_value')
+
+    @api.depends('start_date', 'end_date')
+    def _compute_kpi_hr_recruitment_new_colleagues_value(self):
+        context = self._context
+        for record in self:
+            date_domain = [("create_date", ">=", record.start_date), ("create_date", "<=", record.end_date)]
+            if context.get('timeframe') == 'yesterday':
+                date_domain = [("create_date", ">=", record.start_date), ("create_date", "<", record.end_date)]
+            date_domain += [('company_id', '=', context.get('company_id').id)]
+            new_colleagues = self.env['hr.employee'].search_count(date_domain)
+            record.kpi_hr_recruitment_new_colleagues_value = new_colleagues
