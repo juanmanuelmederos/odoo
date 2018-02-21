3e9d88e6ebfd67953cae5cc56b835471a8ae8c20
Unknown <bst@odoo.com>
2018-02-21 16:53:37 +0100

    [IMP] HR remove unnecessary Error!

--- addons/hr/models/hr.py
+++ addons/hr/models/hr.py
@@ -307,7 +307,7 @@ class Department(models.Model):
     @api.constrains('parent_id')
     def _check_parent_id(self):
         if not self._check_recursion():
-            raise ValidationError(_('Error! You cannot create recursive departments.'))
+            raise ValidationError(_('You cannot create recursive departments.'))
 
     @api.model
     def create(self, vals):
