4a0ef837a0a3723a1443b1112383bab2c34eedb2
Thibault Delavallée <tde@odoo.com>
2018-03-02 12:59:33 +0100

    [REF] mail: new computation for subscribing followers

--- addons/mail/models/mail_followers.py
+++ addons/mail/models/mail_followers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import itertools
+
 from odoo import api, fields, models
 
 
@@ -145,3 +147,152 @@ class Followers(models.Model):
         ('mail_followers_res_channel_res_model_id_uniq', 'unique(res_model,res_id,channel_id)', 'Error, a channel cannot follow twice the same object.'),
         ('partner_xor_channel', 'CHECK((partner_id IS NULL) != (channel_id IS NULL))', 'Error: A follower must be either a partner or a channel (but not both).')
     ]
+
+    # --------------------------------------------------
+    # Private tools methods to fetch followers data
+    # --------------------------------------------------
+
+    def _get_followers_data(self, res_model, res_ids, pids, cids):
+        """ Private method allowing to fetch follower data from a given set of
+        res_model / res_ids. It returns data for each follower: its follower ID,
+        the partner ID (if any), if the partner is shared aka customer or share
+        user (if any partner), the channel ID (if any) and the list of followed
+        subtypes.
+
+         :param doc_data: list of documents to fetch their followers. It is a list
+         of tuples (res_model, res_ids), where res_model is a document model and
+         res_ids the ids of documents.
+
+         :return: list of followers data which is a list of tuples (partner_id
+         (void if channel_id), channel_id (void if partner_id), partner is share
+         (aka no user or shared user, void id channel_id), list of followed subtype
+         ids)
+        """
+        where_params = (res_model, res_ids, pids, cids)
+        query = """
+SELECT fol.id, fol.res_id, fol.partner_id, fol.channel_id, array_agg(subtype.id)
+FROM mail_followers fol
+LEFT JOIN mail_followers_mail_message_subtype_rel fol_rel ON fol_rel.mail_followers_id = fol.id
+LEFT JOIN mail_message_subtype subtype ON subtype.id = fol_rel.mail_message_subtype_id
+WHERE fol.res_model = %s AND fol.res_id = ANY(%s) AND
+    (fol.partner_id = ANY(%s) OR fol.channel_id = ANY(%s))
+GROUP BY fol.id"""
+        self.env.cr.execute(query, where_params)
+        return self.env.cr.fetchall()
+
+    # --------------------------------------------------
+    # Private tools methods to generate new subscription
+    # --------------------------------------------------
+
+    def _insert_followers(self, res_model, res_ids, partner_ids, partner_subtypes, channel_ids, channel_subtypes,
+                          is_customer_ids=None, check_existing=False, existing_policy='skip'):
+        sudo_self = self.sudo()
+        created_ids = list()
+        if not partner_subtypes and not channel_subtypes:  # no subtypes -> default computation, no force, skip existing
+            new, upd = self._add_default_followers(res_model, res_ids, partner_ids, channel_ids, is_customer_ids=is_customer_ids)
+        else:
+            new, upd = self._add_follower_new(res_model, res_ids, partner_ids, partner_subtypes, channel_ids, channel_subtypes, check_existing=check_existing, existing_policy=existing_policy)
+        for res_id, values_list in new.items():
+            for values in values_list:
+                created_ids.append(sudo_self.create(dict(values, res_id=res_id)).id)
+        for fol_id, values in upd.items():
+            sudo_self.browse(fol_id).write(values)
+        return created_ids
+
+    def _add_default_followers(self, res_model, res_ids, partner_ids, channel_ids=None, is_customer_ids=None):
+        """ Private shortcut to add partner ids as follower of documents given
+        their res_model and res_ids. It will compute default subtypes and avoid
+        updating existing followers. """
+        if not partner_ids and not channel_ids:
+            return dict(), dict()
+        channel_ids = channel_ids or []
+
+        default, _, external = self.env['mail.message.subtype'].default_subtypes(res_model)
+        if partner_ids and is_customer_ids is None:
+            is_customer_ids = self.env['res.partner'].sudo().search([('id', 'in', partner_ids), ('partner_share', '=', True)]).ids
+
+        c_stypes = dict.fromkeys(channel_ids, default.ids)
+        p_stypes = dict((pid, external.ids if pid in is_customer_ids else default.ids) for pid in partner_ids)
+
+        return self._add_follower_new(res_model, res_ids, partner_ids, p_stypes, channel_ids, c_stypes, check_existing=True, existing_policy='skip')
+
+    def _add_follower_new(self, res_model, res_ids, partner_ids, partner_subtypes, channel_ids, channel_subtypes,
+                          check_existing=False, existing_policy='skip'):
+        """ Internal method that generates values to insert or update followers.
+        Callers have to handle the result, for example by making a valid ORM
+        command, inserting or updating directly follower records, ...
+
+        This method returns two main data
+
+         * first one is a dict which keys are res_ids. Value is a list of dict of
+           values valid for creating new followers for the related res_id;
+         * second one is a dict which keys are follower ids. Value is a dict of
+           values valid for updating the related follower record;
+
+         :param check_existing: if True, check for existing followers for given
+         documents and handle them according to existing_policy parameter. If False
+         it bypasses the check. If allows to save some computation if caller is sure
+         there are no conflict for followers;
+         :param existing policy: if check_existing is True, tells what to do with
+         already-existing followers:
+
+          * skip: simply skip existing followers, do not touch them;
+          * unlink: unlink existing followers, meaning given values will create
+            new one for all conflicting followers (aka armageddon)
+          * update: give new subtypes, meaning an update dict will be generated
+            to add missing subtypes; no subtypes will ever be removed
+        """
+        doc_pids, doc_cids = dict((res_id, set()) for res_id in res_ids or [0]), dict((res_id, set()) for res_id in res_ids or [0])
+        data_pids, data_cids = dict(), dict()
+
+        new, update = dict(), dict()
+        if check_existing and res_ids:
+            prout = self._get_followers_data(res_model, res_ids, partner_ids or [], channel_ids or [])
+            for fid, rid, pid, cid, sids in prout:
+                if pid:
+                    if existing_policy != 'unlink':
+                        doc_pids[rid].add(pid)
+                    data_pids[fid] = (rid, pid, sids)
+                elif cid:
+                    if existing_policy != 'unlink':
+                        doc_cids[rid].add(cid)
+                    data_cids[fid] = (rid, cid, sids)
+
+            if existing_policy == 'unlink':
+                self.sudo().browse(list(data_pids.keys()) + list(data_cids.keys())).unlink()
+
+        for res_id in res_ids or [0]:
+            for partner_id in set(partner_ids or []):
+                if partner_id not in doc_pids[res_id]:
+                    new.setdefault(res_id, list()).append({
+                        'res_model': res_model,
+                        'partner_id': partner_id,
+                        'subtype_ids': [(6, 0, partner_subtypes[partner_id])],
+                    })
+                elif existing_policy == 'update':
+                    fol_id, sids = next(((key, val[2]) for key, val in data_pids.items() if val[0] == res_id), (False, []))
+                    new_sids = set(partner_subtypes[partner_id]) - set(sids)
+                    if not fol_id or not new_sids:
+                        continue
+                    if new_sids:
+                        update[fol_id] = {
+                            'subtype_ids': [(4, sid) for sid in new_sids]
+                        }
+            for channel_id in set(channel_ids or []):
+                if channel_id not in doc_cids[res_id]:
+                    new.setdefault(res_id, list()).append({
+                        'res_model': res_model,
+                        'channel_id': channel_id,
+                        'subtype_ids': [(6, 0, channel_subtypes[channel_id])],
+                    })
+                elif existing_policy == 'update':
+                    fol_id, sids = next(((key, val[2]) for key, val in data_cids.items() if val[0] == res_id), (False, []))
+                    new_sids = set(channel_subtypes[channel_id]) - set(sids)
+                    if not fol_id or not new_sids:
+                        continue
+                    if new_sids:
+                        update[fol_id] = {
+                            'subtype_ids': [(4, sid) for sid in new_sids]
+                        }
+
+        return new, update
