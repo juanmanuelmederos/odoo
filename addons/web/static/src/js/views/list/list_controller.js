c1d8390f662e09f1b4afb932a7bf5d1d0fc34fc9
Hiral Bhavsar <hbh@odoo.com>
2018-03-06 13:02:20 +0530

    [IMP] web: In list view, added warning popup while archiving selected records

--- addons/web/static/src/js/views/list/list_controller.js
+++ addons/web/static/src/js/views/list/list_controller.js
@@ -10,6 +10,7 @@ odoo.define('web.ListController', function (require) {
 var core = require('web.core');
 var BasicController = require('web.BasicController');
 var DataExport = require('web.DataExport');
+var Dialog = require('web.Dialog');
 var pyeval = require('web.pyeval');
 var Sidebar = require('web.Sidebar');
 
@@ -127,6 +128,12 @@ var ListController = BasicController.extend({
      * @param {jQuery Node} $node
      */
     renderSidebar: function ($node) {
+        var self = this;
+        function confirmDialog() {
+            Dialog.confirm(self, _t("Are you sure that you want to archive all the selected records?"), {
+                confirm_callback: self._onToggleArchiveState.bind(self, true)
+            });
+        }
         if (this.hasSidebar) {
             var other = [{
                 label: _t("Export"),
@@ -135,7 +142,7 @@ var ListController = BasicController.extend({
             if (this.archiveEnabled) {
                 other.push({
                     label: _t("Archive"),
-                    callback: this._onToggleArchiveState.bind(this, true)
+                    callback: confirmDialog
                 });
                 other.push({
                     label: _t("Unarchive"),
